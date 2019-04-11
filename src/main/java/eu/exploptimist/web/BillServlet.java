037bf20257eadca342fab2ec20a47330359d36d1
Divadriel <rei.david26@gmail.com>
2019-04-11 19:19:58 +0200

    added tests for BillServlet, BillServlet generation

--- src/main/java/eu/exploptimist/web/BillServlet.java
+++ src/main/java/eu/exploptimist/web/BillServlet.java
@@ -1,8 +1,6 @@
 package eu.exploptimist.web;
 
-import eu.exploptimist.Fridge;
-import eu.exploptimist.Product;
-import eu.exploptimist.Television;
+import eu.exploptimist.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BillServlet extends HttpServlet {
     List<Product> products = new ArrayList<Product>();
@@ -26,7 +26,49 @@ public class BillServlet extends HttpServlet {
     }
 
     private void displayBill(HttpServletRequest req, HttpServletResponse resp) {
+        Map<String, String> params = splitParameters(req.getQueryString());
+        Customer customer = new Customer(params.get("fullname"), params.get("address"));
+        Delivery delivery = null;
+        switch (params.get("deliveryMode")){
+            case "direct":
+                delivery = new DirectDelivery();
+                break;
+            case "express":
+                delivery = new ExpressDelivery(params.get("deliveryInfo"));
+                break;
+            case "relay":
+                delivery = new RelayDelivery(Integer.parseInt(params.get("deliveryInfo")));
+                break;
+            case "takeAway":
+                delivery = new TakeAwayDelivery();
+                break;
+        }
 
+        Bill bill = new Bill(customer, delivery);
+        String[] productsParams = params.get("products").split("%0D%0A");
+        for (String productLine : productsParams) {
+            String[] productAndQuantity = productLine.split("%3A");
+            Product product = products.get(Integer.parseInt(productAndQuantity[0]));
+            Integer quantity = Integer.parseInt(productAndQuantity[1]);
+            bill.addProduct(product, quantity);
+        }
+
+        bill.generate(new Writer() {
+            @Override
+            public void start() {
+            }
+            @Override
+            public void writeLine(String line) {
+                try {
+                    resp.getWriter().println("<br/>" + line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void stop() {
+            }
+        });
     }
 
     private void displayForm(HttpServletResponse resp) throws IOException {
@@ -61,5 +103,16 @@ public class BillServlet extends HttpServlet {
         products.add(cafe);
         products.add(tv);
         products.add(fridge);
+    }
+
+    public Map<String, String> splitParameters(String queryString) {
+        String[] brutParams = queryString.split("&");
+        Map<String, String> params = new HashMap<>();
+        for (String brutParam : brutParams) {
+            String[] keyAndValue = brutParam.split("=");
+            if (keyAndValue.length == 2)
+                params.put(keyAndValue[0], keyAndValue[1]);
+        }
+        return params;
     }
 }
